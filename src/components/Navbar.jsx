827933763a3d8f0d3ab919b626e88d3f96d154c1
201801392ByronOvalle <3610399800101@ingenieria.usac.edu.gt>
2024-02-25 00:06:32 -0600

    NavBar_Preliminar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import './styles/Navbar.css'
 
 export default function Navbar() {
-
   return (
-    <>
-      <h1>Esto es el navbar</h1>
-    </>
-  )
+    <div className="navbar">
+      <img src="/Logo1.png" alt="Logo" className="navbar-logo" />
+      <nav>
+        <ul>
+          <li><a href="https://create-react-app.dev/docs/getting-started">React.js</a></li>
+          <li><a href="https://vitejs.dev/guide/">Vite.js</a></li>
+          <li><a href="https://nextjs.org/docs">Next.js</a></li>
+          <li><a href="https://tailwindcss.com/docs/installation">Tailwind.css</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
