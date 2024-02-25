acb44a694c36df8dd63f6e7a1f84023f409428cf
201801392ByronOvalle <3610399800101@ingenieria.usac.edu.gt>
2024-02-25 12:01:17 -0600

    NavBar_Completa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,24 @@ export default function Navbar() {
       <img src="/Logo1.png" alt="Logo" className="navbar-logo" />
       <nav>
         <ul>
-          <li><a href="https://create-react-app.dev/docs/getting-started">React.js</a></li>
-          <li><a href="https://vitejs.dev/guide/">Vite.js</a></li>
-          <li><a href="https://nextjs.org/docs">Next.js</a></li>
-          <li><a href="https://tailwindcss.com/docs/installation">Tailwind.css</a></li>
+        <li>
+            <img src="/react.png" alt="React Logo" className="logo" />
+            <a href="https://create-react-app.dev/docs/getting-started">React.js</a>
+          </li>
+          <li>
+            <img src="/vite.png" alt="Vite Logo" className="logo" />
+            <a href="https://vitejs.dev/guide/">Vite.js</a>
+          </li>
+          <li>
+            <img src="/next.png" alt="Next.js Logo" className="logo" />
+            <a href="https://nextjs.org/docs">Next.js</a>
+          </li>
+          <li>
+            <img src="/tailwind.png" alt="Tailwind CSS Logo" className="logo" />
+            <a href="https://tailwindcss.com/docs/installation">Tailwind.css</a>
+          </li>
         </ul>
       </nav>
     </div>
   );
 }
-
